e7cbbdaeb53ef8c66731554b316dcb77ca2f39d0
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-03-25 17:42:48 -0400

    set the bin/bash and set e

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
-cd client \
-&& brew upgrade \
-&& npm install -g cordova ionic \
-&& npm install \
-&& ionic state restore \
+#!/bin/bash
+set -e
+
+cd client
+brew upgrade
+npm install -g cordova ionic
+npm install
+ionic state restore
