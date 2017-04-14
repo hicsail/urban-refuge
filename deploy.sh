26e351a8c5a8f75f4b108d4f9ebd2b2308cffba6
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-04-14 14:35:37 -0400

    removed deploy.sh file, added .idea and prjectFilesBackup/.idea to .gitignore, and removed dependencies pre commands

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-curl \
- -F "status=2" \
- -F "notify=0" \
- -F "ipa=@./platforms/android/build/outputs/apk/android-debug.apk" \
- -H "X-HockeyAppToken: $HockeyAppToken" \
- https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
